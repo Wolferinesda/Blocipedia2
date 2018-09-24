43852b39c4ef8a53bb9ee743eaaa642024ba668a
Wolferinesda <drhalterman1@yahoo.com>
2018-09-24 18:08:40 -0400

    Confirmation email sends and logs the user into the home page when clicking the link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
   devise_for :users, controllers: { confirmations: 'confirmations' }
+  get 'welcome/about', to: 'static_pages#welcome/about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
