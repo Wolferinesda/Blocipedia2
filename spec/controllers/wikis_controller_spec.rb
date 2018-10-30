94830f845e8f78283e319ca42c10f29b9210bbef
Wolferinesda <drhalterman1@yahoo.com>
2018-10-29 21:15:07 -0400

    trying to fix new and edit pages

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -30,14 +30,14 @@ RSpec.describe WikisController, type: :controller do
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
       get :edit, params: {id: @my_wiki.id}
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
