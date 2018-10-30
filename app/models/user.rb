fc048c6ea856c0b1f7387c0c67627cfe50712477
Wolferinesda <drhalterman1@yahoo.com>
2018-10-30 19:06:35 -0400

    Establishing roles for blocipedia site

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  after_initialize :init
+
+  def init
+    self.role ||= :standard
+  end
+
+  validates :name, length: {minimum:1, maximum:100}, presence: true
+  validates :password, presence: true, length: {minimum: 6}, if: -> {"password_digest.nil?"}
+  validates :password, length: {minimum:6}, allow_blank: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum:254}
+
+  has_secure_password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,104 @@
-require 'rails_helper'
+premiumrequire 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_length_of(:email).is_at_least(3) }
+  it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to have_secure_password }
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+  describe "attributes" do
+    it "should have name and email attributes" do
+      expect(user).to have_attributes(name: user.name, email: user.email)
+    end
+
+    it "responds to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "responds to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "responds to standard?" do
+      expect(user).to respond_to(:standard?)
+    end
+
+    it "responds to premium?" do
+      expect(user).to respond_to(:premuim?)
+    end
+  end
+
+  describe "roles" do
+    it "is standard by default" do
+      expect(user.role).to eql("standard")
+    end
+
+    context "standard user" do
+      it "returns true for #standard?" do
+        expect(user.standard?).to be_truthy
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+
+      it "returns false for #premium?" do
+        expect(user.premium?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "returns false for #standard?" do
+        expect(user.standard?).to be_falsey
+      end
+
+      it "returns true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+
+      it "returns false for #premium?" do
+        expect(user.premium?).to be_falsey
+      end
+    end
+
+    context "premium user" do
+      before do
+        user.premium!
+      end
+
+      it "returns false for #standard?" do
+        expect(user.standard?).to be_falsey
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+
+      it "returns true for #premium?" do
+        expect(user.premium?).to be_truthy
+      end
+    end
+  end
+
+  describe "invalid user" do
+    let(:user_with_invalid_name) { build(:user, name: "") }
+    let(:user_with_invalid_email) { build(:user, email: "") }
+
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+  end
 end
