4955e18ee366cc139b9207d569bde72adecbc14f
Wolferinesda <drhalterman1@yahoo.com>
2018-11-07 14:14:53 -0500

    Private wikis function properly

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ChargesController, type: :controller do
-  
+
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
    describe "GET #create" do
     it "returns http success" do
       get :create
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http redirect" do
       get :index
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #about" do
     it "returns http redirect" do
       get :about
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:success)
     end
   end
 
