29d2943bab10dc534fe835ee95a77b38be99f024
Wolferinesda <drhalterman1@yahoo.com>
2018-11-09 22:00:20 -0500

    wiki privacy will automatically be set to false

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,8 @@ class Wiki < ApplicationRecord
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+  
+  before_save { self.private ||= false }
 
   scope :visible_to, -> (user) do
     return all if user.premium? || user.admin?
